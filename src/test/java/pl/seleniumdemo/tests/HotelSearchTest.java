c36130a29e2f5495395cfdf9d8b01cba924254f0
kielbasat <t.kielbasa@tlen.pl>
2022-03-29 14:30:50 +0200

    Added travelers parameters and hotel names assertion modification

--- src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -1,10 +1,9 @@
 package pl.seleniumdemo.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.seleniumdemo.pages.HotelSearchPage;
+import pl.seleniumdemo.pages.ResultsPage;
 
 import java.time.Duration;
 import java.util.List;
@@ -18,20 +17,17 @@ public class HotelSearchTest extends BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
-
         hotelSearchPage.setCity("Dubai");
         hotelSearchPage.setDate("27/03/2022", "29/03/2022");
-        hotelSearchPage.setTravellers();
+        hotelSearchPage.setTravellers(1,1);
         hotelSearchPage.performSearch();
 
-        List<String> hotelNames = driver.findElements(By.cssSelector("h4.list_title"))
-                .stream()
-                .map(el -> el.getAttribute("textContent"))
-                .toList();
-//
-//        Assert.assertEquals(hotelNames.get(0), "Jumeirah Beach Hotel");
-//        Assert.assertEquals(hotelNames.get(1), "Oasis Beach Tower");
-//        Assert.assertEquals(hotelNames.get(2), "Rose Rayhaan Rotana");
-//        Assert.assertEquals(hotelNames.get(3), "Hyatt Regency Perth");
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
+
+        Assert.assertEquals(hotelNames.get(0), "Jumeirah Beach Hotel");
+        Assert.assertEquals(hotelNames.get(1), "Oasis Beach Tower");
+        Assert.assertEquals(hotelNames.get(2), "Rose Rayhaan Rotana");
+        Assert.assertEquals(hotelNames.get(3), "Hyatt Regency Perth");
     }
 }
