4584baf5042517d7480cb57e485385b6306659a2
kielbasat <t.kielbasa@tlen.pl>
2022-03-31 12:39:13 +0200

    Added DriverFactory

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -1,18 +1,20 @@
 package pl.seleniumdemo.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pl.seleniumdemo.utlis.DriverFactory;
+
+import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        driver = DriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
     }
